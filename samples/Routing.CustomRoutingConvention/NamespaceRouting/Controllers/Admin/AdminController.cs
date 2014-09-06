5db4f3b2629b00f6ad4bfb5b929f43ccd247d221
Mugdha Kulkarni <mughdak@microsoft.com>
2014-09-05 17:58:17 -0700

    Creating a sample for MVC issue 693. It shows how to create custom routing conventions

---
+++ samples/Routing.CustomRoutingConvention/NamespaceRouting/Controllers/Admin/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using NamespaceRouting.Models;
+
+namespace MySite.Admin
+{
+    /// <summary>
+    /// Summary description for AdminController
+    /// </summary>
+    public class AdminController : Controller
+    {
+	    public string AddUser(string userName)
+        {
+            return string.Format("User {0} added successfully", userName);
+        }
+    }
+}
---
+++ samples/Routing.CustomRoutingConvention/NamespaceRouting/NameSpaceRoutingConvention.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Mvc.ReflectedModelBuilder;
+using System.Text;
+
+namespace NamespaceRouting
+{
+    internal class NameSpaceRoutingConvention : IReflectedApplicationModelConvention
+    {
+        public void OnModelCreated(ReflectedApplicationModel model)
+        {
+            foreach (var controller in model.Controllers)
+            {
+                if (controller.AttributeRouteModel == null)
+                {
+                    //Create new attribute Route for the controller
+                    controller.AttributeRouteModel = new ReflectedAttributeRouteModel();
+
+                    //Just replace the . in the namespace with a / to create the attribute route 
+                    // Ex: MySite.Admin namespace will corrospond to MySite/Admin attribute route                    
+                    controller.AttributeRouteModel.Template = controller.ControllerType.Namespace.Replace('.', '/');                    
+                }
+
+                //You can continue to put attribute route templates for the controller actions depending on the way you want them to behave
+            }
+        }
+    }
+}
